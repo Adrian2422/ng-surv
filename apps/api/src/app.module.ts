9276ef1549ea2f74c2b319928406b4cfd721e4f2
Adrian Kloc <adrian.kloc47@gmail.com>
2026-02-09 21:02:07 +0100

    feat(api): enhance API with Swagger

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { CqrsModule } from '@nestjs/cqrs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { SurveysModule } from './surveys/surveys.module';
+import { ResponsesModule } from './responses/responses.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    EventEmitterModule.forRoot(),
+    CqrsModule.forRoot(),
+    PrismaModule,
+    SurveysModule,
+    ResponsesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const globalPrefix = 'api';
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix(globalPrefix);
+  const openApiDoc = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('NgSurv')
+      .setDescription('The NgSurv description')
+      .setExternalDoc(
+        'swagger.json',
+        `http://localhost:${port}/${globalPrefix}/swagger.json`,
+      )
+      .setVersion('1.0')
+      .build(),
+  );
+
+  SwaggerModule.setup('api/swagger', app, openApiDoc, {
+    jsonDocumentUrl: '/api/swagger.json',
+  });
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+  Logger.log(
+    `📑 Swagger is running on: http://localhost:${port}/${globalPrefix}/swagger`,
+  );
 }
-bootstrap();
+
+void bootstrap();
